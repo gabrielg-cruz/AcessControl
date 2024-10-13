9744b6b607e231cebe23a5ab81c298e9de700a02
Gabriel <gabrielg-cruz@hotmail.com>
2024-10-13 00:09:25 -0300

    feat: create Area entity

---
+++ accessguard/src/main/java/com/accesscontrol/models/Area.java
@@ -0,0 +1,52 @@
+package com.accesscontrol.models;
+
+import java.util.Set;
+
+import com.accesscontrol.enums.Role;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "area")
+public class Area {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToMany
+    private Set<Role> roles;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+}
