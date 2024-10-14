6ec7e8002a0343639c0c0ae32f0774875d88d20a
Gabriel <gabrielg-cruz@hotmail.com>
2024-10-14 15:35:12 -0300

    feat(services): update implementation of updateEmployee

--- accessguard/src/main/java/com/accesscontrol/services/EmployeeServices.java
+++ accessguard/src/main/java/com/accesscontrol/services/EmployeeServices.java
@@ -3,14 +3,14 @@ package com.accesscontrol.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.accesscontrol.interfaces.IEmployeeService;
+import com.accesscontrol.interfaces.IEmployeeServices;
 import com.accesscontrol.models.Employee;
 import com.accesscontrol.repository.EmployeeRepository;
 
 import jakarta.persistence.EntityNotFoundException;
 
 @Service
-public class EmployeeServices implements IEmployeeService {
+public class EmployeeServices implements IEmployeeServices {
     @Autowired
     private EmployeeRepository employeeRepository;
 
@@ -20,8 +20,23 @@ public class EmployeeServices implements IEmployeeService {
     }
 
     @Override
-    public Employee updateEmployee(Employee employee) {
-        return employeeRepository.save(employee);
+    public Employee updateEmployee(int id, Employee updatedEmployee) {
+        Employee existingEmployee = findEmployeeById(id);
+        if (existingEmployee == null) {
+            throw new EntityNotFoundException("Employee with the id: " + id + "Was not found");
+        }
+
+        existingEmployee
+                .setName(updatedEmployee.getName() != null ? updatedEmployee.getName() : existingEmployee.getName());
+
+        existingEmployee
+                .setEmail(
+                        updatedEmployee.getEmail() != null ? updatedEmployee.getEmail() : existingEmployee.getEmail());
+
+        existingEmployee
+                .setRole(updatedEmployee.getRole() != null ? updatedEmployee.getRole() : existingEmployee.getRole());
+
+        return employeeRepository.save(updatedEmployee);
     }
 
     @Override
@@ -34,5 +49,18 @@ public class EmployeeServices implements IEmployeeService {
     public Employee findEmployeeByEmail(String email) {
         return employeeRepository.findByEmail(email)
                 .orElseThrow(() -> new EntityNotFoundException("Employee with the email: " + email + "Was not found"));
+    }
+
+    @Override
+    public Employee findEmployeeByName(String name) {
+        return employeeRepository.findByName(name)
+                .orElseThrow(() -> new EntityNotFoundException("Employee with the name: " + name + "Was not found"));
+    }
+
+    @Override
+    public Employee deleteEmployee(int id) {
+        Employee employee = findEmployeeById(id);
+        employeeRepository.delete(employee);
+        return employee;
     }
 }
