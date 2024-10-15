38e2b4f0287625993a48d843698639d1efdd1da1
Gabriel <gabrielg-cruz@hotmail.com>
2024-10-15 13:46:41 -0300

    feat(repository): initial implementation of AreaRepository with findByName method

---
+++ accessguard/src/main/java/com/accesscontrol/repository/AreaRepository.java
@@ -0,0 +1,14 @@
+package com.accesscontrol.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.accesscontrol.models.Area;
+
+@Repository
+public interface AreaRepository extends JpaRepository<Area, Integer> {
+
+    Optional<Area> findByName(String name);
+}
