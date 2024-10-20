93783f46f38d2f3cf3701917a613ed4648489b19
Gabriel <gabrielg-cruz@hotmail.com>
2024-10-20 11:20:30 -0300

    feat(controller): add GET opperations to the EmployeeController

--- accessguard/src/main/java/com/accesscontrol/controller/EmployeeController.java
+++ accessguard/src/main/java/com/accesscontrol/controller/EmployeeController.java
@@ -1,15 +1,19 @@
 package com.accesscontrol.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.accesscontrol.dto.EmployeeDTO;
-
+import com.accesscontrol.models.Employee;
 import com.accesscontrol.services.EmployeeServices;
 
 import jakarta.validation.Valid;
@@ -26,5 +30,23 @@ public class EmployeeController {
         EmployeeDTO createdEmployeeDTO = employeeServices.createEmployee(employeeDTO);
 
         return new ResponseEntity<>(createdEmployeeDTO, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        List<Employee> employees = employeeServices.findAll();
+        return ResponseEntity.ok(employees);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EmployeeDTO> getEmployee(@PathVariable int id) {
+        EmployeeDTO employee = employeeServices.findEmployeeById(id);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+
+    @GetMapping("name/{name}")
+    public ResponseEntity<EmployeeDTO> getEmployee(@PathVariable String name) {
+        EmployeeDTO employee = employeeServices.findEmployeeByName(name);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
     }
 }
