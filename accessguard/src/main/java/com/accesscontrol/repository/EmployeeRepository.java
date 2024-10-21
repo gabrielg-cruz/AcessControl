00cc9916d84f595e18b4ec553bddcc16e71c4fc4
Gabriel <gabrielg-cruz@hotmail.com>
2024-10-21 17:56:15 -0300

    feat(repository): add find optional to find methods

--- accessguard/src/main/java/com/accesscontrol/repository/EmployeeRepository.java
+++ accessguard/src/main/java/com/accesscontrol/repository/EmployeeRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.accesscontrol.enums.Role;
 import com.accesscontrol.models.Employee;
 
 @Repository
@@ -14,4 +15,6 @@ public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     Optional<Employee> findByEmail(String email);
 
     Optional<Employee> findByName(String name);
+
+    Optional<Employee> findByRole(Role role);
 }
