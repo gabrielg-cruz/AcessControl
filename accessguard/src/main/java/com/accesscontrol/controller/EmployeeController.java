7db4b73459c3c164bb533029ee744f064d890eb0
Gabriel <gabrielg-cruz@hotmail.com>
2024-10-21 17:54:34 -0300

    feat(controllers): add CRUD methods to EmployeeController

--- accessguard/src/main/java/com/accesscontrol/controller/EmployeeController.java
+++ accessguard/src/main/java/com/accesscontrol/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,5 +49,11 @@ public class EmployeeController {
     public ResponseEntity<EmployeeDTO> getEmployee(@PathVariable String name) {
         EmployeeDTO employee = employeeServices.findEmployeeByName(name);
         return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable int id) {
+        employeeServices.deleteEmployee(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
