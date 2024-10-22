0d4a1e18bfe5380f4308aa17f9a2fae443bd779d
Gabriel <gabrielg-cruz@hotmail.com>
2024-10-22 18:45:32 -0300

    feat(DTO): add Area DTO

---
+++ accessguard/src/main/java/com/accesscontrol/dto/AreaDTO.java
@@ -0,0 +1,39 @@
+package com.accesscontrol.dto;
+
+import java.util.Set;
+
+import com.accesscontrol.enums.Role;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.validation.constraints.NotBlank;
+
+public class AreaDTO {
+    private int id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @ElementCollection(targetClass = Role.class)
+    private Set<String> roles;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+}
