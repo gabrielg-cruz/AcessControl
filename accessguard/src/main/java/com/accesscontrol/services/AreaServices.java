d4736f00aefcc2b8a23679e825e6e8caf7547d24
Gabriel <gabrielg-cruz@hotmail.com>
2024-10-24 11:47:06 -0300

    feat(services): Add DTO usage in AreaServices

--- accessguard/src/main/java/com/accesscontrol/services/AreaServices.java
+++ accessguard/src/main/java/com/accesscontrol/services/AreaServices.java
@@ -1,9 +1,13 @@
 package com.accesscontrol.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.accesscontrol.dto.AreaDTO;
 import com.accesscontrol.interfaces.IAreaServices;
+import com.accesscontrol.mapper.AreaMapper;
 import com.accesscontrol.models.Area;
 import com.accesscontrol.repository.AreaRepository;
 
@@ -15,26 +19,48 @@ public class AreaServices implements IAreaServices {
     private AreaRepository areaRepository;
 
     @Override
-    public Area createArea(Area area) {
-        return areaRepository.save(area);
+    public AreaDTO createArea(AreaDTO areaDTO) {
+        Area area = AreaMapper.INSTANCE.toEntity(areaDTO);
+        Area createdArea = areaRepository.save(area);
+        return AreaMapper.INSTANCE.toDTO(createdArea);
     }
 
     @Override
-    public Area updateArea(Area area) {
-        return areaRepository.save(area);
+    public AreaDTO updateArea(int id, AreaDTO updatedArea) {
+        Area existingArea = findAreaEntityById(id);
+        if (existingArea == null) {
+            throw new EntityNotFoundException("Area with the id: " + id + "Was not found");
+        }
+
+        existingArea.setName(updatedArea.getName() != null ? updatedArea.getName() : existingArea.getName());
+
+        existingArea.setRoles(updatedArea.getRoles() != null ? updatedArea.getRoles() : existingArea.getRoles());
+
+        return AreaMapper.INSTANCE.toDTO(areaRepository.save(existingArea));
     }
 
     @Override
-    public Area findAreaById(int id) {
+    public Area findAreaEntityById(int id) {
         return areaRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Area with the id: " + id + "Was not found"));
     }
 
     @Override
-    public Area deleteArea(int id) {
-        Area area = findAreaById(id);
-        areaRepository.delete(area);
-        return area;
+    public AreaDTO findAreaById(int id) {
+        Area area = areaRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Area with the id: " + id + "Was not found"));
+        return AreaMapper.INSTANCE.toDTO(area);
     }
 
+    @Override
+    public List<Area> findAll() {
+        return areaRepository.findAll();
+    }
+
+    @Override
+    public AreaDTO deleteArea(int id) {
+        Area area = findAreaEntityById(id);
+        areaRepository.delete(area);
+        return AreaMapper.INSTANCE.toDTO(area);
+    }
 }
