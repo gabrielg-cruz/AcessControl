7acd0d8ae4553f51464dfb46a4d0e9a426c7c442
Gabriel <gabrielg-cruz@hotmail.com>
2024-10-24 13:13:28 -0300

    feat(controller): add findById method

--- accessguard/src/main/java/com/accesscontrol/controller/AreaController.java
+++ accessguard/src/main/java/com/accesscontrol/controller/AreaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,5 +33,11 @@ public class AreaController {
     public ResponseEntity<List<AreaDTO>> getAllAreas() {
         List<AreaDTO> areas = areaServices.findAll();
         return ResponseEntity.ok(areas);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AreaDTO> getAreaById(@PathVariable int id) {
+        AreaDTO area = areaServices.findAreaById(id);
+        return ResponseEntity.ok(area);
     }
 }
