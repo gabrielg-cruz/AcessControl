66396f9e00a054416711a2820319ddf4851d1577
Gabriel <gabrielg-cruz@hotmail.com>
2024-10-24 12:49:00 -0300

    feat(controllers): add getAll method to AreaController

--- accessguard/src/main/java/com/accesscontrol/controller/AreaController.java
+++ accessguard/src/main/java/com/accesscontrol/controller/AreaController.java
@@ -1,8 +1,11 @@
 package com.accesscontrol.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +26,11 @@ public class AreaController {
     public ResponseEntity<AreaDTO> createArea(@Valid @RequestBody AreaDTO areaDTO) {
         AreaDTO createdAreaDTO = areaServices.createArea(areaDTO);
         return new ResponseEntity<>(createdAreaDTO, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AreaDTO>> getAllAreas() {
+        List<AreaDTO> areas = areaServices.findAll();
+        return ResponseEntity.ok(areas);
     }
 }
