780ab3a8e8727366b6f7914147809543e3c59b74
Gabriel <gabrielg-cruz@hotmail.com>
2024-10-26 00:27:17 -0300

    refactor(repository): remove join query from AreaRepository

--- accessguard/src/main/java/com/accesscontrol/repository/AreaRepository.java
+++ accessguard/src/main/java/com/accesscontrol/repository/AreaRepository.java
@@ -3,8 +3,6 @@ package com.accesscontrol.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.accesscontrol.models.Area;
@@ -13,7 +11,4 @@ import com.accesscontrol.models.Area;
 public interface AreaRepository extends JpaRepository<Area, Integer> {
 
     Optional<Area> findByName(String name);
-
-    @Query("SELECT a FROM Area a LEFT JOIN FETCH a.roles WHERE a.id = :id")
-    Optional<Area> findByIdWithRoles(@Param("id") int id);
 }
