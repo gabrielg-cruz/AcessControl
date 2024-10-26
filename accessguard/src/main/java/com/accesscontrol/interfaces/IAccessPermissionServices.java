7143a4f96fb33f18a3970102b789278f84e9f1ef
Gabriel <gabrielg-cruz@hotmail.com>
2024-10-26 20:03:41 -0300

    fix(interfaces): fix typo in class name

--- accessguard/src/main/java/com/accesscontrol/interfaces/IAccessPermissionServices.java
+++ accessguard/src/main/java/com/accesscontrol/interfaces/IAccessPermissionServices.java
@@ -3,7 +3,7 @@ package com.accesscontrol.interfaces;
 import com.accesscontrol.models.Area;
 import com.accesscontrol.models.Employee;
 
-public interface IAccessPemissionServices {
+public interface IAccessPermissionServices {
     public boolean isMaintenance(Employee employee);
 
     public boolean isAllowed(Employee employee, Area area);
