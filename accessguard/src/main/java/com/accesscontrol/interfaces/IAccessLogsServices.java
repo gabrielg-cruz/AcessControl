bdd2a3e231d103bb5822f84ab1e91b0910a0b7a0
Gabriel <gabrielg-cruz@hotmail.com>
2024-10-26 19:03:07 -0300

    feat(interfaces): add Employee id to parameters of methods

--- accessguard/src/main/java/com/accesscontrol/interfaces/IAccessLogsServices.java
+++ accessguard/src/main/java/com/accesscontrol/interfaces/IAccessLogsServices.java
@@ -2,13 +2,14 @@ package com.accesscontrol.interfaces;
 
 import com.accesscontrol.dto.AccessLogsDTO;
 import com.accesscontrol.models.AccessLogs;
+import com.accesscontrol.models.Employee;
 
 public interface IAccessLogsServices {
     AccessLogs findAccessLogsEntityById(int id);
 
     AccessLogsDTO findAccessLogsById(int id);
 
-    AccessLogsDTO createAccessLogs(AccessLogsDTO accessLogsDTO);
+    AccessLogsDTO createAccessLogs(Employee employee, AccessLogsDTO accessLogsDTO);
 
     AccessLogsDTO updateAccessLogs(int id, AccessLogsDTO accessLogsDTO);
 
