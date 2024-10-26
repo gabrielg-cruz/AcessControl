7f6409ecbb790757bd5269fc590cd556bbf461bb
Gabriel <gabrielg-cruz@hotmail.com>
2024-10-26 01:10:07 -0300

    feat(mapper): add mapper to AccessLogs entity and DTO

--- accessguard/src/main/java/com/accesscontrol/mapper/AccessLogsMapper.java
+++ accessguard/src/main/java/com/accesscontrol/mapper/AccessLogsMapper.java
@@ -1,16 +1,22 @@
 package com.accesscontrol.mapper;
 
+import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import com.accesscontrol.dto.AccessLogsDTO;
 import com.accesscontrol.models.AccessLogs;
 
+@Mapper
 public interface AccessLogsMapper {
     AccessLogsMapper INSTANCE = Mappers.getMapper(AccessLogsMapper.class);
 
+    @Mapping(target = "employeeId", source = "employee.id")
+    @Mapping(target = "areaId", source = "area.id")
     AccessLogsDTO toDTO(AccessLogs accessLogs);
 
     @Mapping(target = "id", ignore = true)
+    @Mapping(target = "employee.id", source = "employeeId")
+    @Mapping(target = "area.id", source = "areaId")
     AccessLogs toEntity(AccessLogsDTO accessLogsDTO);
 }
