ca1db9787e86b5aa9f495a04e7f361ac27bd0ac0
Gabriel <gabrielg-cruz@hotmail.com>
2024-11-04 12:33:12 -0300

    feat(controller): add Delete route in AccessLogs controller

--- accessguard/src/main/java/com/accesscontrol/controller/AcessLogsController.java
+++ accessguard/src/main/java/com/accesscontrol/controller/AcessLogsController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -56,4 +57,9 @@ public class AcessLogsController {
         return new ResponseEntity<>(accessLogs, HttpStatus.OK);
     }
 
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<String> deleteAccessLog(@PathVariable int id) {
+        accessLogsServices.deleteAccessLogs(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
